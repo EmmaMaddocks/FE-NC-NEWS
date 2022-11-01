11dc0812bec385693142e53ea242c182b9decdb9
Emma Maddocks <emma.l.maddocks@gmail.com>
2022-11-01 12:08:24 +0000

    adds basic styling to buttons

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,24 +1,29 @@
 import { Link } from "react-router-dom";
 
 const ArticleCard = ({ article }) => {
-    const { title, img_url, author, topic, created_at, article_id, comment_count } = article;
+  const {
+    title,
+    img_url,
+    author,
+    topic,
+    created_at,
+    article_id,
+    comment_count,
+  } = article;
 
-    return (
+  return (
+    <li key={article_id} className="article-card">
+      <div className="article-details">
+        <p>Comments: {comment_count}</p>
+        <p>Posted by {author} </p>
+        <p>{created_at}</p>
+      </div>
+      <h2>{title}</h2>
+      <Link to={`/articles/${article_id}`}>
+        <button>Read Article</button>
+      </Link>
+    </li>
+  );
+};
 
-        <li key={article_id} className="article-card">
-<div className="article-details">
-          <p>Comments: {comment_count}</p>
-          <p>Posted by {author} </p>
-          <p>{created_at}</p>
-          </div>
-          <h2>{title}</h2>
-          <Link to={`/articles/${article_id}`}>
-            <button>Read Article</button>
-          </Link>
-        </li>
-
-      );
-    }
-    
-    export default ArticleCard;
-    
+export default ArticleCard;
