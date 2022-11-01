50f30557341884e26b8a9cf0754177cc6715f825
Emma Maddocks <emma.l.maddocks@gmail.com>
2022-11-01 12:01:19 +0000

    added button linking article card to full article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 const ArticleCard = ({ article }) => {
     const { title, img_url, author, topic, created_at, article_id, comment_count } = article;
 
     return (
+
         <li key={article_id} className="article-card">
 <div className="article-details">
           <p>Comments: {comment_count}</p>
@@ -9,7 +12,11 @@ const ArticleCard = ({ article }) => {
           <p>{created_at}</p>
           </div>
           <h2>{title}</h2>
+          <Link to={`/articles/${article_id}`}>
+            <button>Read Article</button>
+          </Link>
         </li>
+
       );
     }
     
