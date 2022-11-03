77a107ccf17f54c6149c88ac63e3590b8dd6e7af
Emma Maddocks <emma.l.maddocks@gmail.com>
2022-11-03 16:27:46 +0000

    changed homepage welcome

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -6,9 +6,11 @@ function HomeHeader({loggedInUser}) {
     return (
         <>
     <div class='home-header'>
-        <h2>Welcome to NCNews {loggedInUser ? loggedInUser : null}!</h2>
+        <h2>Welcome to NCNews!</h2>
         <p class='welcome'>
         Providing you with the latest news from the Northcoders community</p>
+        {loggedInUser ? <h3>You are currently logged in as {loggedInUser}</h3> : null}
+
     </div>
     <div className="collections">
     <RecentArticles/>
