4208c766f33dc48e8d88840295d6c4011cdf5d0a
Emma Maddocks <emma.l.maddocks@gmail.com>
2022-12-08 14:58:49 +0000

    adds dialog box to delete button

--- src/components/DeleteComment.jsx
+++ src/components/DeleteComment.jsx
@@ -1,8 +1,25 @@
-import { useEffect } from "react";
+import { useState } from "react";
 import { BsTrash } from "react-icons/bs";
 import * as api from '../utils/api'
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
 
 const DeleteCommentByUser = ({ id }) => {
+
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
 
   const deleteComment = () => {
     api.deleteComment(id)
@@ -13,12 +30,34 @@ const DeleteCommentByUser = ({ id }) => {
 
 
     return (
+      <>
         <BsTrash color="rgb(3, 169, 152)" size={16}
           className="deleteButton"
-          onClick={() => {
-            deleteComment();
+          onClick={handleClickOpen}
+ />
 
-          }} />
+          <Dialog
+ open={open}
+ onClose={handleClose}
+ aria-labelledby="alert-dialog-title"
+ aria-describedby="alert-dialog-description"
+>
+
+ <DialogContent>
+   <DialogContentText id="alert-dialog-description">
+  Are you sure you want to delete this comment?
+   </DialogContentText>
+ </DialogContent>
+ <DialogActions>
+   <Button onClick={handleClose}>Cancel</Button>
+   <Button   onClick={() => {
+    deleteComment();
+  }} autoFocus>
+     I'm sure
+   </Button>
+ </DialogActions>
+</Dialog>
+</>
     );
   
 };
